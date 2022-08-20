84a552edfecc7b2d162746853559ed8d99406047
ga1az <ga1az@outlook.com>
2022-08-20 13:10:18 -0400

    add

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 const { getAllChampions, getOneChampion } = require("./dataSet");
 
@@ -8,6 +10,10 @@ app.get("/", (req, res) => {
 
 app.get("/:name", (req, res) => {
   res.send(getOneChampion(req.params.name));
+});
+
+app.get("/picture", (req, res) => {
+  res.sendFile(path.join(__dirname, "data/champions/tft7_aatrox.tft_set7.png"));
 });
 
 app.listen(3000, () => {
